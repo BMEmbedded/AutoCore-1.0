bfac5f25b0c54292badf60771a7c688573320400
Carrova-Automotive <126565725+Carrova-Automotive@users.noreply.github.com>
2023-04-19 17:18:32 +0100

    Update Lookup Table.ino

--- Power Management Unit/Battery percentage/Lookup Table.ino
+++ Power Management Unit/Battery percentage/Lookup Table.ino
@@ -1,78 +1,71 @@
-float batteryVoltage;
-
-int batteryPercentage;
-
+int readPin=A3;
+int blueLight=10;
+int GreenLight=12;
+int RedLight=14;
+int readVal;
+float V2=0;
+int delayTime=1000;
+int percentage;
 void setup() {
-
-  // initialize analog input pin
-
-  pinMode(A0, INPUT);
-
+  // put your setup code here, to run once:
+pinMode(readPin,INPUT); 
+pinMode(blueLight,OUTPUT)
+Serial.begin(9600);
 }
 
 void loop() {
-
-  // read battery voltage
-
-  batteryVoltage = analogRead(A0) * (5.0 / 1023.0) * 4.0; // assuming 4x voltage divider
-
-  // lookup battery percentage
-
-  if (batteryVoltage >= 12.7) {
-
-    batteryPercentage = 100;
-
-  } else if (batteryVoltage >= 12.5) {
-
-    batteryPercentage = 90;
-
-  } else if (batteryVoltage >= 12.2) {
-
-    batteryPercentage = 80;
-
-  } else if (batteryVoltage >= 12.0) {
-
-    batteryPercentage = 70;
-
-  } else if (batteryVoltage >= 11.8) {
-
-    batteryPercentage = 60;
-
-  } else if (batteryVoltage >= 11.6) {
-
-    batteryPercentage = 50;
-
-  } else if (batteryVoltage >= 11.4) {
-
-    batteryPercentage = 40;
-
-  } else if (batteryVoltage >= 11.1) {
-
-    batteryPercentage = 30;
-
-  } else if (batteryVoltage >= 10.8) {
-
-    batteryPercentage = 20;
-
-  } else if (batteryVoltage >= 10.5) {
-
-    batteryPercentage = 10;
-
-  } else {
-
-    batteryPercentage = 0;
-
-  }
-
-  // print battery percentage
-
-  Serial.print("Battery Percentage: ");
-
-  Serial.print(batteryPercentage);
-
-  Serial.println("%");
-
-  delay(1000); // wait for 1 second before taking the next reading
-
+  // put your main code here, to run repeatedly:
+readVal=analogRead(readPin);
+V2=(5./1023.)*readval;
+delay(delayTime);
+if(5>v2>4.92){
+  percentage=90%;
+  Serial.println(percentage);
+  digitalWrite(blueLight,HIGH);
 }
-
+if(4,92>v2>4.88){
+  percentage=80%;
+  Serial.println(percentage);
+  digitalWrite(blueLight,HIGH);
+}
+if(4,88>v2>4.85){
+  percentage=70%;
+  Serial.println(percentage);
+  digitalWrite(GlueLight,HIGH);
+}
+if(4,85>v2>4.8){
+  percentage=60%;
+  Serial.println(percentage);
+  digitalWrite(GreenLight,HIGH);
+}
+if(4,8>v2>4.74){
+  percentage=50%;
+  Serial.println(percentage);
+  digitalWrite(GreenLight,HIGH);
+}
+if(4,74>v2>4.68){
+  percentage=40%;
+  Serial.println(percentage);
+  digitalWrite(GreenLight,HIGH);
+}
+if(4,68>v2>4.64){
+  percentage=30%;
+  Serial.println(percentage);
+  digitalWrite(GreenLight,HIGH);
+}
+if(4,64>v2>4.55){
+  percentage=20%;
+  Serial.println(percentage);
+  digitalWrite(RedLight,HIGH);
+}
+if(4,55>v2>4.45){
+  percentage=10%;
+  Serial.println(percentage);
+  digitalWrite(RedLight,HIGH);
+}
+if(4,45>v2>4.13){
+  percentage=00%;
+  Serial.println(percentage);
+  digitalWrite(RedLight,HIGH);
+}
+}
